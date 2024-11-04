bcb8ef01f78d1553eb70ff49d18904414ce3fdd0
Your Name <you@example.com>
2024-11-04 15:24:38 +0700

    pi calc example

--- dags/submit_pi_calc.sh
+++ dags/submit_pi_calc.sh
@@ -1,3 +1,4 @@
+# ./Scripts/submit_gno_fraud_install.sh
 #!/usr/bin/env bash
 set -e
 
@@ -21,35 +22,32 @@ export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
 export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/nativ"
 cd ${AIRFLOW_HOME}
 
-# if [ ! -f environment.tar.gz ]
-# then
-#     conda create -y -n pyspark-airflow  python=3.8 sqlalchemy=2.0.21 psycopg2=2.9.3 conda-pack pandas=1.5.2 joblib=1.4.2   
-#     source /opt/conda/bin/activate pyspark-airflow
-#     pip install gspread==6.1.2
-#     pip install pydrive==1.3.1
-#     conda pack -o environment.tar.gz
-# fi
-# conda unpack -o environment.tar.gz
-
-
+if [ ! -f environment.tar.gz ]
+then
+    conda create -y -n pyspark-airflow  python=3.8 sqlalchemy=2.0.21 psycopg2=2.9.3 conda-pack pandas=1.5.2 joblib=1.4.2
+    source /opt/conda/bin/activate pyspark-airflow
+    pip install gspread==6.1.2
+    pip install pydrive==1.3.1
+    conda pack -o environment.tar.gz
+fi
 
 # source /home/airflow/.bashrc
 # Spark Related Options
 export SPARK_HOME="/opt/spark"
 # export PYSPARK_PYTHON=./environment/bin/python
 EXECUTION_DATE=${1}
-
 echo "Execution Date: ${EXECUTION_DATE}"
 
+
 spark-submit \
---master yarn \
---deploy-mode cluster \
---name Demo::${EXECUTION_DATE} \
---driver-memory 4g \
---driver-cores 4 \
---executor-memory 4g \
---executor-cores 4 \
---num-executors 3 \
-${AIRFLOW_HOME}/dags/repo/dags/pi-spark.py
+    --master yarn \
+    --deploy-mode cluster \
+    --name DP::Demo::Pi::${EXECUTION_DATE} \
+    --driver-memory 4g \
+    --driver-cores 4 \
+    --executor-memory 4g \
+    --executor-cores 4 \
+    --num-executors 3 \
+    ${AIRFLOW_HOME}/dags/repo/dags/pi-spark.py
 
 # --archives environment.tar.gz#environment \
