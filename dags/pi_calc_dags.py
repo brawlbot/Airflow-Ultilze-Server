dcbd4285d73c77fa3d287cbd2edd4a22a7c0e32e
Your Name <you@example.com>
2024-11-05 14:03:23 +0700

    pi calc example

--- dags/pi_calc_dags.py
+++ dags/pi_calc_dags.py
@@ -34,10 +34,28 @@ with DAG(
     tags=['IRD'],
 ) as dag:
 
-    step_pi_calc = BashOperator(
-        task_id='pi_calc',
-        bash_command=f"""bash {AIRFLOW_HOME}/dags/repo/dags/submit_pi_calc.sh""",
-        dag=dag
-    )
+    # step_pi_calc = BashOperator(
+    #     task_id='pi_calc',
+    #     # bash_command=f"""bash {AIRFLOW_HOME}/dags/repo/dags/submit_pi_calc.sh""",
+    #     bash_command="""
+    #         echo "Execution Date: ${EXECUTION_DATE}"
+    #     """,
+    #     dag=dag
+    # )
 
-    step_pi_calc
+
+    step_spark_submit_pi = BashOperator(
+        task_id='spark_submit_pi',
+        bash_command="""spark-submit \
+            --class org.apache.spark.examples.SparkPi \
+            --master yarn --deploy-mode client \
+            --driver-cores 2 \
+            --driver-memory 1g \
+            --num-executors 3 \
+            --executor-cores 2 \
+            --executor-memory 1g \
+            /opt/spark/examples/jars/spark-examples*.jar 10""",
+    )
+    # step_pi_calc
+
+    step_spark_submit_pi
