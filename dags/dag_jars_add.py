0a4ae4a4c4b044c30a2e06c1eba8ec4bf6e7b00c
Your Name <you@example.com>
2024-11-12 15:55:59 +0700

    verify jdbc connect to postgresql

---
+++ dags/dag_jars_add.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import pendulum
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+AIRFLOW_HOME = os.environ.get("AIRFLOW_HOME")
+
+if AIRFLOW_HOME is None:
+    AIRFLOW_HOME = "/opt/airflow"
+# [END import_module]
+local_tz = pendulum.timezone("Asia/Ho_Chi_Minh")
+
+start_date= datetime(2024, 10, 31, 15, 0, tzinfo=local_tz)
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    'verify_jdbc_version',
+    default_args=default_args,
+    description='Verify JDBC version',
+    schedule_interval="25 15 * * *",
+    start_date=start_date,
+    catchup=False,
+    tags=['IRD'],
+) as dag:
+
+
+    custom = BashOperator(
+        task_id='verify_jdbc_version',
+        bash_command="""spark-submit \
+            --master yarn --deploy-mode client \
+            --driver-cores 2 \
+            --driver-memory 1g \
+            --num-executors 3 \
+            --executor-cores 2 \
+            --executor-memory 1g \
+            --archives hdfs://c0s/user/dp-ai-workspace-97ta9/archives/pydoop_env.tar.gz#environment \
+            --jars hdfs://c0s/user/dp-ai-workspace-97ta9/jars/postgresql-42.7.4.jar \
+            /opt/airflow/dags/repo/scripts/verify_jdbc_version.py
+        """,
+
+        env={
+            "HADOOP_CLIENT_OPTS": "-Xmx2147483648 -Djava.net.preferIPv4Stack=true",
+            "HADOOP_CONF_DIR": "/etc/hadoop",
+            "PYSPARK_PYTHON": "./environment/bin/python",
+            "PATH": "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/hadoop/bin:/opt/hive/bin:/opt/spark/bin",
+            "PYTHONPATH": "/opt/spark/python/lib/py4j-0.10.7-src.zip:/opt/spark/python/lib/py4j-0.10.9-src.zip:/opt/spark/python/lib/py4j-0.10.9.3-src.zip:/opt/spark/python:$PYTHONPATH",
+            "PYTHONSTARTUP": "/opt/spark/python/pyspark/shell.py",
+            "SPARK_HOME": "/opt/spark",
+            "SPARK_CONF_DIR": "/etc/spark",
+        }
+    )
---
+++ scripts/verify_jdbc_version.py
@@ -0,0 +1,35 @@
+from time import time
+import os
+from random import random
+from operator import add
+import numpy as np
+import pydoop.hdfs as hdfs
+import json
+
+from pyspark.sql import SparkSession
+
+os.environ['PYSPARK_PYTHON'] = "./environment/bin/python"
+spark = SparkSession.builder.appName('VerifyJDBCVersion').getOrCreate()
+sc = spark.sparkContext
+
+
+# print(f"Spark version: {spark.version}")
+# print(f"JDBC version: {spark.conf.get('spark.jars')}")
+
+db_cred_fn = "hdfs://c0s/user/dp-ai-workspace-97ta9/config/postgres.json"
+with hdfs.open(db_cred_fn) as f:
+    cfg = json.load(f)
+
+DBUSER = cfg.get('Connect DB')['DBUSER']
+DBPASS = cfg.get('Connect DB')['DBPASS']
+DBHOST = cfg.get('Connect DB')['DBHOST']
+DBPORT = cfg.get('Connect DB')['DBPORT']
+DBNAME = cfg.get('Connect DB')['DBNAME']
+
+df = spark.read.jdbc(url=f"jdbc:postgresql://{DBHOST}:{DBPORT}/{DBNAME}",
+                     table="public.tmp_random_data", properties={"user": DBUSER, "password": DBPASS})
+
+df_local = df.limit(10).toPandas()
+
+print(df_local)
+print(df_local.shape)
